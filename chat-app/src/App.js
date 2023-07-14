4e297d97660f90775fba0a3d8bcca88a36f6aadd
rohitcodism <rohitpaula18i@gmail.com>
2023-07-14 19:49:39 +0530

    do it

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <h1>User: {name}</h1>
+      <div className='chat-container'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong> name: </strong>
+          <span>chat message</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
