b11a68360a94dbae03bdc0b02d1df5dea8722034
rohitcodism <rohitpaula18i@gmail.com>
2023-07-14 21:52:57 +0530

    do it

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 
 function App() {
 
-  const [name, setName] = useState('user1');
+  const [name, setName] = useState('');
   const [chats, setChats] = useState([{name: 'user1', message: 'message1'}, { name: 'user2', message: 'message2' }]);
   const [msg, setmsg] = useState('');
   const sendChat = ()=>{
     const c = [...chats];
     c.push({name, message: msg});
     setChats(c);
+    setmsg('');
   }
 
   return (
     <div className='chat-contain'>
+      { name ? null : <div className='owner'>
+        <input type="text" placeholder='Enter your name' onBlur={e => {const title = e.target.value}} />
+        <button onClick={() => setName()}>Chat</button>
+      </div>}
       <h3>User: {name}</h3>
       <div className='chat-container'>
           {chats.map(c => <div className={` container  ${c.name === name ? 'me' : ''}`}>
@@ -25,7 +30,7 @@ function App() {
           )}
       </div>
       <div className="message-box">
-        <input type="text" placeholder='Type your message' onInput={ e => setmsg(e.target.value)} />
+        <input type="text" placeholder='Type your message' onInput={ e => setmsg(e.target.value)} value={msg} />
         <button className='send' onClick={e => sendChat()}>Send</button>
       </div>
     </div>
