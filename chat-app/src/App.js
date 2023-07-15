60b34932c8a61a23866f901867006d5c6a0374b9
rohitcodism <rohitpaula18i@gmail.com>
2023-07-15 10:14:17 +0530

    do it

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,25 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getDatabase } from "firebase/database";
+import {  ref, push, set, onChildAdded } from "firebase/database";
 
 function App() {
 
   const [name, setName] = useState('');
-  const [chats, setChats] = useState([{name: 'user1', message: 'message1'}, { name: 'user2', message: 'message2' }]);
+  const [chats, setChats] = useState([]);
   const [msg, setmsg] = useState('');
+
+
+
+  const database = getDatabase();
+  const chatListRef = ref(database, 'chat');
+
+  useEffect(() => {
+    onChildAdded(chatListRef, (data) => {
+      console.log(data.val());
+    });
+  })
+
+
+
   const sendChat = ()=>{
-    const c = [...chats];
-    c.push({name, message: msg});
-    setChats(c);
+    const newChatRef = push(chatListRef);
+    set(newChatRef, {
+      name, message: msg
+    });
+
+    // const c = [...chats];
+    // c.push();
+    // setChats(c);
     setmsg('');
   }
 
   return (
     <div className='chat-contain'>
       { name ? null : <div className='owner'>
-        <input type="text" placeholder='Enter your name' onBlur={e => {const title = e.target.value}} />
-        <button onClick={() => setName()}>Chat</button>
+        <input type="text" placeholder='Enter your name' onBlur={e => setName(e.target.value)} />
       </div>}
-      <h3>User: {name}</h3>
+      { name ? <div>
+        <h3>User: {name}</h3>
       <div className='chat-container'>
           {chats.map(c => <div className={` container  ${c.name === name ? 'me' : ''}`}>
               <p className={`chat-box  ${c.name === name ? 'me' : ''}`}>
@@ -33,6 +54,7 @@ function App() {
         <input type="text" placeholder='Type your message' onInput={ e => setmsg(e.target.value)} value={msg} />
         <button className='send' onClick={e => sendChat()}>Send</button>
       </div>
+      </div> : null}
     </div>
   );
 }
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCI9KP_K_j_QDnSX7nn94k4sZCM9s-JhWc",
+  authDomain: "chat-app-774eb.firebaseapp.com",
+  databaseURL: "https://chat-app-774eb-default-rtdb.firebaseio.com",
+  projectId: "chat-app-774eb",
+  storageBucket: "chat-app-774eb.appspot.com",
+  messagingSenderId: "97984376729",
+  appId: "1:97984376729:web:40e8d8fc01726076ab6d08"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
