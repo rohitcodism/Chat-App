2bd53f6c1fd99d63005ec4d8940a45b7826d7a87
rohitcodism <rohitpaula18i@gmail.com>
2023-07-15 20:32:46 +0530

    do it

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -7,27 +7,25 @@ function App() {
 
   const [name, setName] = useState('');
   const [chats, setChats] = useState([]);
-  const [msg, setmsg] = useState('');
+  const [msg, setMsg] = useState('');
 
+  const db = getDatabase();
+  const chatListRef = ref(db, 'chats');
 
-
-  const database = getDatabase();
-  const chatListRef = ref(database, 'chat');
-
-  useEffect(()=>{
+  useEffect(() => {
     onChildAdded(chatListRef, (data) => {
-      setChats(chats => [...chats, data.val()]);
-    });    
-  },[])
-
-
+        setChats(chats => [...chats, data.val()]);
+      });
+  }, [])
 
   const sendChat = ()=>{
-    const newChatRef = push(chatListRef);
-    set(newChatRef, {
+
+    
+    const chatRef = push(chatListRef);
+    set(chatRef, {
       name, message: msg
     });
-    setmsg('');
+    setMsg('');
   }
 
   return (
@@ -37,20 +35,21 @@ function App() {
       </div>}
       { name ? <div>
         <h3>User: {name}</h3>
-      <div className='chat-container'>
-          {chats.map((c, i) => <div key={i}  className={` container  ${c.name === name ? 'me' : ''}`}>
-              <p className={`chat-box  ${c.name === name ? 'me' : ''}`}>
+        <div className='chat-container'>
+          {chats.map((c , i) => (<div key={i} className={` container ${c.name === name ? 'me' : ''}`}>
+              <p className={`chat-box ${c.name === name ? 'me' : ''}`}>
                 <strong> {c.name} </strong>
                 <span>{ c.message }</span>
+                <span>{i}</span>
               </p>
             </div> 
-          )}
+          ))}
       </div>
       <div className="message-box">
-        <input type="text" placeholder='Type your message' onInput={ e => setmsg(e.target.value)} value={msg} />
+        <input type="text" placeholder='Type your message' onInput={ e => setMsg(e.target.value)} value={msg} />
         <button className='send' onClick={e => sendChat()}>Send</button>
       </div>
-      </div> : null}
+      </div> : null }
     </div>
   );
 }
