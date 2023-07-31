5d336861082a9cb479fe345b1384214d37031b8e
rohitcodism <rohitpaula18i@gmail.com>
2023-07-31 08:26:52 +0530

    Good Morning

--- chitchat/src/components/ChatFeed.jsx
+++ chitchat/src/components/ChatFeed.jsx
@@ -1,4 +1,16 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./Mymessage";
+import TheirMessage from "./TheirMessage";
+
 const ChatFeed =  ()=> {
+    const { chats, activeChat, userName, messages } = props;
+    
+    const chat = chats && chats[activeChat]
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+        console.log(keys);
+    }
     return(
         <div>
             ChatFeed
