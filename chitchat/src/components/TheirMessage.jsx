3f82dd1822529577c62ffcb5dc39c62eb7b3118b
rohitcodism <rohitpaula18i@gmail.com>
2023-07-31 09:54:47 +0530

    do it

--- chitchat/src/components/TheirMessage.jsx
+++ chitchat/src/components/TheirMessage.jsx
@@ -1,10 +1,23 @@
 const TheirMessage = ({lastMessage, message}) => {
 
-    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
 
     return(
-        <div>
-            Their Message
+        <div className="message-row">
+            { isFirstMessageByUser && (
+                <div
+                    className="message-avatar"
+                    style={{backgroundImage: `url(${message?.sender?.avatar})`}}
+                />
+            )}
+
+            {message?.attachments?.length > 0 ? (
+                <img src={message.attachments[0].file} alt="message-attachment" className="message-name" style={{marginLeft: isFirstMessageByUser ? '4px' : '48px'}} />
+            ):(
+                <div className="message" style={{float: 'right', marginRight: '18px', color:'white', backgroundColor: '#3B2A50'}}>
+                    {message.text}
+                </div>
+            )}
         </div>
     );
 }
